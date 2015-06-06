2d11a0b371646c234a71df18f9cda71ae03dd80e
WebFreak001 <janju007@web.de>
2015-06-06 18:29:34 +0200

    added quick conversion functions for NBTItems

--- source/nbtd/INBTItem.d
+++ source/nbtd/INBTItem.d
@@ -1,5 +1,9 @@
 module nbtd.INBTItem;
 
+import nbtd;
+
+import std.conv;
+
 enum NBTType : ubyte
 {
 	End = cast(ubyte)0,
@@ -32,4 +36,16 @@ interface INBTItem
 	void decode(ubyte[] data, bool compressed = true, bool hasName = true);
 
 	void read(ref ubyte[] stream, bool hasName = true);
+
+	final NBTByte asByte() { assert(type == NBTType.Byte, "Expected Byte, got " ~ to!string(type)); return cast(NBTByte)this; }
+	final NBTShort asShort() { assert(type == NBTType.Short, "Expected Short, got " ~ to!string(type)); return cast(NBTShort)this; }
+	final NBTInt asInt() { assert(type == NBTType.Int, "Expected Int, got " ~ to!string(type)); return cast(NBTInt)this; }
+	final NBTLong asLong() { assert(type == NBTType.Long, "Expected Long, got " ~ to!string(type)); return cast(NBTLong)this; }
+	final NBTFloat asFloat() { assert(type == NBTType.Float, "Expected Float, got " ~ to!string(type)); return cast(NBTFloat)this; }
+	final NBTDouble asDouble() { assert(type == NBTType.Double, "Expected Double, got " ~ to!string(type)); return cast(NBTDouble)this; }
+	final NBTByteArray asByteArray() { assert(type == NBTType.ByteArray, "Expected ByteArray, got " ~ to!string(type)); return cast(NBTByteArray)this; }
+	final NBTString asString() { assert(type == NBTType.String, "Expected String, got " ~ to!string(type)); return cast(NBTString)this; }
+	final NBTList asList() { assert(type == NBTType.List, "Expected List, got " ~ to!string(type)); return cast(NBTList)this; }
+	final NBTCompound asCompound() { assert(type == NBTType.Compound, "Expected Compound, got " ~ to!string(type)); return cast(NBTCompound)this; }
+	final NBTIntArray asIntArray() { assert(type == NBTType.IntArray, "Expected IntArray, got " ~ to!string(type)); return cast(NBTIntArray)this; }
 }
